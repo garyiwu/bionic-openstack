a8e784ef3b44cbf4ee828dccb900bd0fb436d51c
gwu <gary.wu@gmail.com>
2018-07-11 15:01:15 -0700

    keystone installation

--- controller.sh
+++ controller.sh
@@ -10,7 +10,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 source passwords.sh
 
-apt -y install python3-openstackclient
+apt -y install python3-openstackclient crudini
 
 apt -y install mariadb-server python3-pymysql
 
@@ -74,3 +74,55 @@ WantedBy=multi-user.target
 EOF
 systemctl enable etcd
 systemctl start etcd
+
+
+mysql -sfu root <<EOF
+CREATE DATABASE keystone;
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$KEYSTONE_DBPASS';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$KEYSTONE_DBPASS';
+EOF
+apt -y install keystone  apache2 libapache2-mod-wsgi
+crudini --set /etc/keystone/keystone.conf database connection "mysql+pymysql://keystone:$KEYSTONE_DBPASS@controller/keystone"
+crudini --set /etc/keystone/keystone.conf token provider fernet
+su -s /bin/sh -c "keystone-manage db_sync" keystone
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage bootstrap --bootstrap-password $ADMIN_PASS \
+  --bootstrap-admin-url http://$IP_ADDR:5000/v3/ \
+  --bootstrap-internal-url http://$IP_ADDR:5000/v3/ \
+  --bootstrap-public-url http://$IP_ADDR:5000/v3/ \
+  --bootstrap-region-id RegionOne
+sed -i '/ServerRoot/a ServerName controller' /etc/apache2/apache2.conf
+service apache2 restart
+
+cat > ~/admin-openrc <<EOF
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=$ADMIN_PASS
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+EOF
+
+cat > ~/demo-openrc <<EOF
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=$DEMO_PASS
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+EOF
+
+source ~/admin-openrc
+openstack domain create --description "An Example Domain" example
+openstack project create --domain default --description "Service Project" service
+openstack project create --domain default --description "Demo Project" demo
+openstack user create --domain default --password $DEMO_PASS demo
+openstack role create user
+openstack role add --project demo --user demo user
+openstack token issue
+
