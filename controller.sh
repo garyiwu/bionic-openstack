a8025ddf2f91ab260755e035f474543bcfacc170
gwu <gary.wu@gmail.com>
2018-07-11 18:33:43 -0700

    heat installation

--- controller.sh
+++ controller.sh
@@ -503,3 +503,71 @@ service nova-api restart
 service cinder-scheduler restart
 service apache2 restart
 openstack volume service list
+
+#
+# Heat
+#
+mysql -fu root <<EOF
+CREATE DATABASE heat;
+GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'localhost' IDENTIFIED BY '$HEAT_DBPASS';
+GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'%' IDENTIFIED BY '$HEAT_DBPASS';
+EOF
+
+source ~/admin-openrc
+openstack user create --domain default --password $HEAT_PASS heat
+openstack role add --project service --user heat admin
+openstack service create --name heat --description "Orchestration" orchestration
+openstack service create --name heat-cfn --description "Orchestration" cloudformation
+openstack endpoint create --region RegionOne orchestration public http://$IP_ADDR:8004/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne orchestration internal http://$IP_ADDR:8004/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne orchestration admin http://$IP_ADDR:8004/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne cloudformation public http://$IP_ADDR:8000/v1
+openstack endpoint create --region RegionOne cloudformation internal http://$IP_ADDR:8000/v1
+openstack endpoint create --region RegionOne cloudformation admin http://$IP_ADDR:8000/v1
+openstack domain create --description "Stack projects and users" heat
+openstack user create --domain heat --password $HEAT_DOMAIN_PASS heat_domain_admin
+openstack role add --domain heat --user-domain heat --user heat_domain_admin admin
+openstack role create heat_stack_owner
+openstack role add --project demo --user demo heat_stack_owner
+openstack role create heat_stack_user
+apt-get -y install heat-api heat-api-cfn heat-engine
+
+crudini --merge /etc/heat/heat.conf <<EOF
+[database]
+connection = mysql+pymysql://heat:$HEAT_DBPASS@controller/heat
+
+[DEFAULT]
+transport_url = rabbit://openstack:$RABBIT_PASS@controller
+heat_metadata_server_url = http://controller:8000
+heat_waitcondition_server_url = http://controller:8000/v1/waitcondition
+stack_domain_admin = heat_domain_admin
+stack_domain_admin_password = $HEAT_DOMAIN_PASS
+stack_user_domain_name = heat
+
+[keystone_authtoken]
+auth_url = http://controller:5000
+memcached_servers = controller:11211
+auth_type = password
+project_domain_name = default
+user_domain_name = default
+project_name = service
+username = heat
+password = $HEAT_PASS
+
+[trustee]
+auth_type = password
+auth_url = http://controller:5000
+username = heat
+password = $HEAT_PASS
+user_domain_name = default
+
+[clients_keystone]
+auth_uri = http://controller:5000
+EOF
+su -s /bin/sh -c "heat-manage db_sync" heat
+service heat-api restart
+service heat-api-cfn restart
+service heat-engine restart
+
+source ~/admin-openrc
+openstack orchestration service list
