1895efefb587f89b3b2aa12beaf282e9a4313812
Gary Wu <gary.wu@gmail.com>
2018-07-23 16:07:39 -0700

    upload trusty image

--- controller.sh
+++ controller.sh
@@ -231,6 +231,13 @@ openstack image create "xenial" \
   --disk-format qcow2 --container-format bare \
   --public
 
+wget https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img -P ~/
+openstack image create "trusty" \
+  --file ~/trusty-server-cloudimg-amd64-disk1.img \
+  --disk-format qcow2 --container-format bare \
+  --public
+
+
 openstack image list
 
 #
