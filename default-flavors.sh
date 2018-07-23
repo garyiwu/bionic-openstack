db26b4b3ede07d9bdb5d606c679b9ae1b06cb56d
Gary Wu <gary.wu@gmail.com>
2018-07-23 16:08:19 -0700

    add c1.xlarge flavor

--- default-flavors.sh
+++ default-flavors.sh
@@ -5,3 +5,4 @@ openstack flavor create --id 2 --vcpus 1 --disk 20 --ram 2048 m1.small
 openstack flavor create --id 3 --vcpus 2 --disk 40 --ram 4096 m1.medium
 openstack flavor create --id 4 --vcpus 4 --disk 80 --ram 8192 m1.large
 openstack flavor create --id 5 --vcpus 8 --disk 160 --ram 16384 m1.xlarge
+openstack flavor create --id 6 --vcpus 8 --disk 40 --ram 16384 c1.xlarge
