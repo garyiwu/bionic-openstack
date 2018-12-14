4c15406f8849ce69cf629b5f8d6f4a775213a88c
Gary Wu <gary.wu@gmail.com>
2018-12-14 12:52:44 -0800

    add cinder v1 endpoints

--- controller.sh
+++ controller.sh
@@ -473,8 +473,12 @@ EOF
 source ~/admin-openrc
 openstack user create --domain default --password $CINDER_PASS cinder
 openstack role add --project service --user cinder admin
+openstack service create --name cinder --description "OpenStack Block Storage" volume
 openstack service create --name cinderv2 --description "OpenStack Block Storage" volumev2
 openstack service create --name cinderv3 --description "OpenStack Block Storage" volumev3
+openstack endpoint create --region RegionOne volume public http://$IP_ADDR:8776/v1/%\(project_id\)s
+openstack endpoint create --region RegionOne volume internal http://$IP_ADDR:8776/v1/%\(project_id\)s
+openstack endpoint create --region RegionOne volume admin http://$IP_ADDR:8776/v1/%\(project_id\)s
 openstack endpoint create --region RegionOne volumev2 public http://$IP_ADDR:8776/v2/%\(project_id\)s
 openstack endpoint create --region RegionOne volumev2 internal http://$IP_ADDR:8776/v2/%\(project_id\)s
 openstack endpoint create --region RegionOne volumev2 admin http://$IP_ADDR:8776/v2/%\(project_id\)s
