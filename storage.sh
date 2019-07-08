8d71a2e83bdd2406a72a94e499a444cb550f1326
Gary Wu <gary.wu@gmail.com>
2019-07-08 13:41:53 -0700

    change storage to use NFS driver

--- storage.sh
+++ storage.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
 
-# need to manually run the following:
+# for lvm, need to manually run the following:
 # pvcreate /dev/sdb
 # vgcreate cinder-volumes /dev/sdb
+
+# for NFS, export mount at localhost:/cinder-volumes
 
 if [ "$#" -ne 1 ]; then
     export IP_ADDR=$(hostname -I | cut -d' ' -f1)
@@ -15,18 +17,19 @@ export DEBIAN_FRONTEND=noninteractive
 source passwords.sh
 
 add-apt-repository -y cloud-archive:pike
-apt update
-apt -y dist-upgrade
+apt-get update
+apt-get -y dist-upgrade
 
 
-apt -y install crudini
+apt-get -y install crudini
+
 
 #
 # Cinder
 #
-apt -y install lvm2 thin-provisioning-tools
+apt-get -y install lvm2 thin-provisioning-tools
 
-apt -y install cinder-volume
+apt-get -y install cinder-volume
 
 crudini --merge /etc/cinder/cinder.conf <<EOF
 [database]
@@ -36,7 +39,7 @@ connection = mysql+pymysql://cinder:$CINDER_DBPASS@controller/cinder
 transport_url = rabbit://openstack:$RABBIT_PASS@controller
 auth_strategy = keystone
 my_ip = $IP_ADDR
-enabled_backends = lvm
+enabled_backends = nfs
 glance_api_servers = http://controller:9292
 
 [keystone_authtoken]
@@ -55,6 +58,11 @@ volume_group = cinder-volumes
 iscsi_protocol = iscsi
 iscsi_helper = tgtadm
 volume_clear_size=50
+
+[nfs]
+volume_driver = cinder.volume.drivers.nfs.NfsDriver
+nas_host = localhost
+nas_share_path = /cinder-volumes
 
 [oslo_concurrency]
 lock_path = /var/lib/cinder/tmp
